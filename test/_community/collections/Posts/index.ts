39702e3da88a491465c1150ae8e8f39160a008a1
Amelia Lim <limchornguan418@gmail.com>
2025-01-22 14:15:40 +0100

    Access control incorrectly applied to unrelated fields

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -4,13 +4,56 @@ export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
   slug: postsSlug,
-  admin: {
-    useAsTitle: 'title',
-  },
   fields: [
     {
       name: 'title',
       type: 'text',
+    },
+    {
+      name: 'readOnlyContent',
+      type: 'text',
+      access: {
+        update: () => false,
+      },
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'Tab 1',
+          name: 'tab1',
+          fields: [
+            {
+              name: 'links1',
+              label: 'links1',
+              type: 'array',
+              fields: [
+                {
+                  name: 'linkLabel',
+                  type: 'text',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: 'Tab 2',
+          name: 'tab2',
+          fields: [
+            {
+              name: 'links2',
+              label: 'links2',
+              type: 'array',
+              fields: [
+                {
+                  name: 'linkLabel',
+                  type: 'text',
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
   ],
   versions: {
